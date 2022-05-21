4770709c94e9fdf71fd6a9ba0b2f40f8253ffbc8
Awais2323 <awais66990@outlook.com>
2022-05-22 01:43:37 +0500

    change database connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "root1234",
-  DB: "recruiterAgency",
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b79ac077dca2a5",
+  PASSWORD: "de80cc52",
+  DB: "heroku_14f574871f9493b",
   dialect: "mysql",
   pool: {
     max: 5,
